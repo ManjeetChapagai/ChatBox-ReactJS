dcfcdd9b20b48d5444f834d565f008b8cb71c6b3
ManjeetChapagai <74332423+ManjeetChapagai@users.noreply.github.com>
2021-03-09 03:53:49 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { ChatEngine } from "react-chat-engine";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <h1> I am Chat Box</h1>
+    // <ChatEngine
+    //     height = '100vh'
+    //     projectID = ""
+    //     userName = ""
+    //     userSecret =""
+    // />
+  );
+};
+export default App;
